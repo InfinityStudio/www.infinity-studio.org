6df76eff67ece28a298667d67e583eff6b1a936f
LasmGratel <lasm_gratel@hotmail.com>
2015-10-31 17:22:04 +0800

    fix change language

--- index.php
+++ index.php
@@ -26,15 +26,14 @@
 require 'init.php';
 if(isset($_COOKIE["language"])) {
     $smarty->assign('language',$_COOKIE["language"]);
-} else {
-    if($_GET["languages"]=="zh_CN") {
-        setcookie("language","zh_CN");
-    }
-    elseif($_GET["languages"]=="en_US") {
-        setcookie("language","en_US");
-    }
-    $smarty->assign('language',$_COOKIE["language"]);
 }
+if($_GET["languages"]=="zh_CN") {
+    setcookie("language","zh_CN");
+}
+elseif($_GET["languages"]=="en_US") {
+    setcookie("language","en_US");
+}
+$smarty->assign('language',$_COOKIE["language"]);
 $smarty->assign('title','Infinity Studio');
 $smarty->assign('mainActive', 'active');
 $smarty->assign('bodyClass','bs-docs-home');
