ac30c57755cd7e37d9ee946496f547561907123d
CannonFotter <guofanyi49@126.com>
2015-11-01 19:41:04 +0800

    fix db.class.php but still bad

---
+++ command/controller/Controller.class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright (C) 2015 Infinity Studio.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ *
+ * @author LasmGratel <lasm_gratel@hotmail.com>
+ * @author Cannon_fotter <gfyguofanyi@gmail.com>
+ */
+class Controller{
+    protected $_smarty = null;
+    protected $_model = null;
+    protected function __construct(&$_smarty,$_model = null){
+        $this->_smarty = $_smarty;
+        $this->_model = $_model;
+    }
+}
--- command/lib/DB.class.php
+++ command/lib/DB.class.php
@@ -1,10 +1,24 @@
 <?php
-
 /**
- * Created by Cannon_fotter.
- * Date: 2015年10月31日 0031
- * Time: 11:24
- * Project: html
+ * Copyright (C) 2015 Infinity Studio.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ *
+ * @author LasmGratel <lasm_gratel@hotmail.com>
+ * @author Cannon_fotter <gfyguofanyi@gmail.com>
  */
 class DB{
 
@@ -23,28 +37,31 @@ class DB{
 
     public function __construct() {
         self::$dbtype = 'mysql';
-        self::$dbhost = '';
+        self::$dbhost = '127.0.0.1';
         self::$dbport = '3306';
-        self::$dbname = 'tion';
-        self::$dbuser = '';
-        self::$dbpass = '';
+        self::$dbname = 'test';
+        self::$dbuser = 'root';
+        self::$dbpass = 'root';
         self::$connect = false;
         self::$charset = 'UTF8';
         self::connect ();
         self::$DB->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         self::$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        self::$DB->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
 //        self::$DB->setAttribute ( PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true );
 //        self::$DB->setAttribute ( PDO::ATTR_EMULATE_PREPARES, true );
-        self::execute ( 'SET NAMES ' . self::$charset );
+        //self::execute ( 'SET NAMES ' . self::$charset );
     }
     public function __destruct() {
         self::close ();
     }
     public function connect() {
         try {
-            self::$DB = new PDO ( self::$dbtype . ':host=' . self::$dbhost . ';port=' . self::$dbport . ';dbname=' . self::$dbname, self::$dbuser, self::$dbpass, array (
-                PDO::ATTR_PERSISTENT => self::$connect
-            ) );
+//            self::$DB = new PDO(self::$dbtype.':host='.self::$dbhost.';port='.self::$dbport.';dbname='.self::$dbname.self::$dbuser,self::$dbpass,array(
+//                PDO::ATTR_PERSISTENT => self::$connect
+//            ));
+//            self::$dbtype.':host='.self::$dbhost.';port='.self::$dbport.';dbname='.self::$dbname,self::$dbuser,self::$dbpass
+            self::$DB = new PDO('mysql:host=localhost;dbname=test','root','root');
         } catch ( PDOException $e ) {
             die ( "Connect Error Infomation:" . $e->getMessage() );
         }
@@ -54,17 +71,19 @@ class DB{
     }
     public function exec($sql,$args) {
         self::getPDOError($sql);
-        self::$DB->prepare($sql);
-        return self::$DB->execute($args);
+        $a = self::$DB->prepare($sql);//->execute($args);
+        //if ($a->execute($args) == false) echo 'fuck!';
+        return $a->execute($args);
+        //return self::$DB->execute($args);
     }
-    public function fetch($sql,$args,$type=1) {
+    public function fetch($sql,$args=array(),$type=1) {
         $result = array();
         self::$stmt = self::exec($sql,$args);
         self::getPDOError($sql);
-        self::$stmt->setFetchMode(PDO::FETCH_OBJ);
+
         switch ($type) {
             case '0' :
-                $result = self::$stmt->fetch();
+                $result = self::$stmt->fetch(PDO::FETCH_ASSOC);
                 break;
             case '1' :
                 $result = self::$stmt->fetchAll();
